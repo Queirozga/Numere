54028998e39bd9132c3a3d8c2cd324553d33fda2
Queirozga <bielhenri1302@gmail.com>
2025-05-06 08:02:20 -0300

    começando as funções de add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 let form = document.querySelector('#form')
 let submit = document.querySelector('#submit')
+let input = document.querySelector('#iadv')
 
-form.addEventListener('submit', trying)
+form.addEventListener('submit', tentativa)
 
-function trying(e) {
+function tentativa(e) {
     e.preventDefault();
-
-    let correct = Math.random();
+    
+    let correct = (Math.random() * 100).toFixed(0);
     console.log(correct)
+}
+
+input.addEventListener('Keyup', checaEnter);
+
+const checaEnter = (e) => {
+    if (e.key === 'Enter') tentativa();
 }
